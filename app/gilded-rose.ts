e10888faf4e9ffc7d64a3434980695ed6ad29e2b
Andrey Klenkov <50375131+wx6boy@users.noreply.github.com>
2021-11-10 20:55:04 +0300

    updated

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,3 +1,7 @@
+import Item, {ItemNamesEnum} from './Item'
+
+
+
 export class GildedRose {
     items: Array<Item>;
 
@@ -5,53 +9,60 @@ export class GildedRose {
         this.items = items;
     }
 
-    updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
+    updateQuality() : Array<Item> {
+        for (let item of this.items) {
+            switch (item.name) {
+                case ItemNamesEnum.sulfuras:
+                    break;
+                case ItemNamesEnum.conjured:    
+                    item.sellIn -= 1;
+                    if ( item.sellIn < 0 && item.quality > 1) {
+                        item.quality -= 2;
                     }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
+                    else if ( item.quality > 0 ) {
+                        item.quality -= 1;
                     }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
+                    break
+                case ItemNamesEnum.agedBrie:
+                    item.sellIn -= 1;
+                    if ( item.sellIn < 0 && item.quality < 49 ) {
+                        item.quality += 2;
+                    } 
+                    else if ( item.quality < 50 ) {
+                        item.quality += 1;
                     }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
+                    break;
+                case ItemNamesEnum.backstagePasses:
+                    item.sellIn -= 1;
+                    if ( item.quality  >= 50 ) {
+                        break;
                     }
-                }
+                    if ( item.sellIn < 0 ) {
+                        item.quality = 0;
+                        break;
+                    }
+                    if ( item.sellIn < 5 ) {
+                        item.quality = Math.min(item.quality + 3, 50);
+                        break;
+                    }
+                    if ( item.sellIn < 10 ) {
+                        item.quality = Math.min(item.quality + 2, 50);
+						break;
+                    }
+                    item.quality += 1;
+					break; 
+                default: 
+                    item.sellIn -= 1;
+                    if (item.sellIn < 0 && item.quality > 1) {
+                        item.quality -= 2;
+                    }
+                    else if ( item.quality > 0 ) {
+                        item.quality -= 1;
+                    }
+                    break
             }
         }
-
+    
         return this.items;
     }
 }

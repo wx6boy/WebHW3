60e1965fc5ebd28d1568c96de3f1b6910a381e79
Andrey Klenkov <50375131+wx6boy@users.noreply.github.com>
2021-11-10 19:31:44 +0300

    updated (remove item)

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,15 +1,3 @@
-export class Item {
-    name: string;
-    sellIn: number;
-    quality: number;
-
-    constructor(name, sellIn, quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
-}
-
 export class GildedRose {
     items: Array<Item>;
 

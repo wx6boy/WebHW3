632389cdd9a74577ea9a37b9c3b20bb49b45179c
Andrey Klenkov <50375131+wx6boy@users.noreply.github.com>
2021-11-10 21:36:45 +0300

    Update gilded-rose.spec.ts

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { GildedRose } from '../app/gilded-rose'
+import { Item } from '../app/Item';
 
 describe('Gilded Rose', function () {
 
@@ -7,6 +8,30 @@ describe('Gilded Rose', function () {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].name).to.equal('fixme');
+    });
+    
+    it('Backstage 1', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 8, 8)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(10);
+    });
+    
+    it('Backstage 2', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 13, 13)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(12);
+    });
+    
+    it('Aged Brie 1', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 15, 15)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(16);
+    });
+    
+    it('Aged Brie 2', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 15, 15)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(13);
     });
 
 });
